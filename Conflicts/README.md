1c8d9b4752016d007f0aaca1c5ecd9f00f3d507c
netodotcom <neetopriv@gmail.com>
2023-03-31 13:14:57 -0300

    Conflict example

---
+++ Conflicts/README.md
@@ -0,0 +1,3 @@
+# threads-conflicts
+
+Real conflict between threads when they share a resource (In this case -> Counter) and attempt to access or modify it simultaneously without a proper synchronization.
---
+++ Conflicts/TwoThreads.java
@@ -0,0 +1,20 @@
+package Conflicts;
+
+public class TwoThreads {
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+        Thread thread1 = new Thread(new MyThread(counter));
+        Thread thread2 = new Thread(new MyThread(counter));
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Result: " + counter.getCount());
+    }
+}
