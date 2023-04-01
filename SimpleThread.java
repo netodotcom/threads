f471f1629788fe8c245da0bdea2660c44a75f149
neto <netodotcom@proton.me>
2023-04-01 18:34:06 -0300

    deleting some files

--- SimpleThread.java
+++
@@ -1,8 +0,0 @@
-public class SimpleThread {
-
-    public static void main(String args[]) {
-        RunnableImplementation runnableImplementation = new RunnableImplementation();
-        Thread myThread = new Thread(runnableImplementation);
-        myThread.start();
-    }
-}
--- TwoThreads.java
+++
@@ -1,8 +0,0 @@
-public class TwoThreads {
-    public static void main(String[] args) {
-        Thread thread1 = new Thread(new RunnableImplementation("Thread 1"));
-        Thread thread2 = new Thread(new RunnableImplementation("Thread 2"));
-        thread1.start();
-        thread2.start();
-    }
-}
